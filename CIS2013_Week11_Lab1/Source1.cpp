c5b9ebdab1f9359301d5ce5f406d9298ef74d2e0
Daniel Novotny <svc_okc_bt204@ad.osuokc.edu>
2018-04-04 20:17:34 -0500

    2

--- CIS2013_Week11_Lab1/Source1.cpp
+++ CIS2013_Week11_Lab1/Source1.cpp
@@ -3,29 +3,42 @@
 using namespace std;
 
 class BirthDate {
-public:
+private:
 	int year;
 	int month;
 	int day;
+
+public:
 	void print() {
 		cout << month << "/" << day << "/" << year;
+	}
+
+	void set(int d, int m, int y) {
+		year = y;
+		month = m;
+		day = d;
 	}
 };
 
 int main(){
 
 	BirthDate bob;
-	cout << "Enter the Bobs birthday day: ";
+	/*cout << "Enter the Bobs birthday day: ";
 	cin >> bob.day;
 
 	cout << "Enter the Bobs birthday month: ";
 	cin >> bob.month;
 
 	cout << "Enter the Bobs birthday year: ";
-	cin >> bob.year;
+	cin >> bob.year;*/
 
 	cout << "Bobs dirthday is ";
 	bob.print;
 	cout << endl;
+
+	BirthDate tom;
+	tom.set(1, 2, 1992);
+	tom.print;
+
 }
 
